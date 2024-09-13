bd50d176e138da79c3f78ded7d15c5520416c974
EastonA01 <eastonredwing@gmail.com>
2024-09-13 12:30:00 -0400

    Modified comments

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,8 +7,8 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/is_it_friday_yet.feature", // Path to the specific feature file
-        glue = {"steps"}, // Package where your step definitions are located
-        plugin = {"pretty"} // Optional: This will print the test results in a readable format
+        glue = {"steps"}, // Package where step definitions are located
+        plugin = {"pretty"} // This makes the output prettier :D
 )
 public class TestRunner {
 }
